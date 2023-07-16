6fdbe049b3603429ef33c47e5e4a5ff7c499e886
ajgeiss0702 <ajgeiss72@gmail.com>
2023-07-16 16:37:53 -0500

    Add ratelimiting to dev requests

--- src/worker.ts
+++ src/worker.ts
@@ -15,8 +15,22 @@ export interface Env {
 	YOUTUBE_KEY: string
 }
 
+const rateLimit: {
+	[key: string]: number
+} = {};
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const connectingIp = request.headers.get('cf-connecting-ip');
+		if(!connectingIp) {
+			return Response.json({message: "Missing IP"}, {status: 400});
+		}
+		const lastRequest = rateLimit[connectingIp] || 0;
+
+		if(Date.now() - lastRequest < 500) {
+			return Response.json({message: "Too many requests! Slow down!"}, {status: 429});
+		}
+
 		const id = await env.WHENISWAN_FETCHER.idFromName("dev")
 		const stub = env.WHENISWAN_FETCHER.get(id);
 		return stub.fetch(request.url)
