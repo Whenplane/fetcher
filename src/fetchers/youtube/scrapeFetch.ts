c7741fad8f501d670ca181732a9f643f53a26e2b
ajgeiss0702 <ajgeiss72@gmail.com>
2023-07-29 18:06:04 -0700

    slightly decrease 5 second cache to be within the 5-second browser update interval

--- src/fetchers/youtube/scrapeFetch.ts
+++ src/fetchers/youtube/scrapeFetch.ts
@@ -6,7 +6,7 @@ const COUNT_VALUE = "scrape_livecount:count";
 
 export async function getLiveCount(state: DurableObjectState, env: Env) {
 	const lastFetch: number = (await state.storage.get(COUNT_LASTFETCH)) || 0;
-	if(Date.now() - lastFetch < 5e3) { // cache for 5 seconds
+	if(Date.now() - lastFetch < 4750) { // cache for 5 seconds
 		return (await state.storage.get(COUNT_VALUE) as number) || 0;
 	}
 	state.storage.put(COUNT_LASTFETCH, Date.now());
