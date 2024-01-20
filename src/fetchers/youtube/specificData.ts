81ab96d693bd77e60e8ac8e79315e9cd92470eaf
ajgeiss0702 <ajgeiss72@gmail.com>
2024-01-19 22:06:12 -0700

    yes

--- src/fetchers/youtube/specificData.ts
+++ src/fetchers/youtube/specificData.ts
@@ -16,7 +16,7 @@ export async function getSpecificData(state: DurableObjectState, id: string, env
 	let cacheTime = 15 * 60e3;
 
 	if(cachedValue) {
-		console.log({cachedValue})
+		console.log(JSON.stringify(cachedValue, undefined, '\t'))
 		for (let item of cachedValue) {
 			if(!item.snippet.title.includes("WAN")) continue;
 			// if(item.snippet.liveBroadcastContent !== "live") break;
