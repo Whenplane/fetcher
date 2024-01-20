c181cb07838229bf34b7d4923d11f5d419ec0690
ajgeiss0702 <ajgeiss72@gmail.com>
2024-01-19 21:27:08 -0700

    hopefully fix cache thing and not break woohoo friday prod push

--- src/fetchers/youtube/specificData.ts
+++ src/fetchers/youtube/specificData.ts
@@ -2,13 +2,61 @@ import { Env } from '../../worker';
 import { getKey, v } from './index';
 import { get, put } from '../../storageCacher';
 
+
+let lastMissingStartTimeSend = 0;
+
 export async function getSpecificData(state: DurableObjectState, id: string, env: Env) {
 	const LASTFETCH = "api_specific:" + id + ":lastFetch";
 	const LASTDATA = "api_specific:" + id + ":data";
 
 	const lastFetch: number = (await get(state, LASTFETCH)) || 0;
-	if(Date.now() - lastFetch < 15 * 60e3) {
-		return await get(state, LASTDATA);
+
+	const cachedValue = await get(state, LASTDATA);
+
+	let cacheTime = 15 * 60e3;
+
+	for (let item of cachedValue) {
+		if(!item.snippet.title.includes("WAN")) continue;
+		if(item.snippet.liveBroadcastContent !== "live") break;
+		if(!item.liveStreamingDetails?.actualStartTime) {
+			// wtf youtube why do you make me do this
+			cacheTime = 10e3; // if the stream is live but there is no start time, try again in 10 seconds.
+
+			// send an alert if this happens with the data
+			if(env.DISCORD_WEBHOOK && Date.now() - lastMissingStartTimeSend > 10e3) { // limit to one message every 10 seconds
+				v((async () => {
+					if(!env.DISCORD_WEBHOOK) return;
+
+					const formData = new FormData();
+
+					formData.append("payload_json", JSON.stringify(
+						{
+							content: `Missing actualStartTime`
+						}
+					));
+
+					formData.append(
+						"files[0]",
+						new Blob(
+							[JSON.stringify(cachedValue, undefined, '\t')],
+							{type: 'application/json'}
+						),
+						"items.json"
+					)
+
+					await fetch(env.DISCORD_WEBHOOK, {
+						method: "POST",
+						body: formData,
+					})
+					lastMissingStartTimeSend = Date.now();
+				})())
+			}
+		}
+	}
+
+
+	if(Date.now() - lastFetch < cacheTime) {
+		return cachedValue;
 	}
 
 	v(put(state, LASTFETCH, Date.now()));
