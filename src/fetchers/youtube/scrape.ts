5f8934e5b023c1a965405922890dcef3feb2e10a
ajgeiss0702 <aj@ajg0702.us>
2024-09-15 17:11:41 -0700

    add more randomness to fetching

--- src/fetchers/youtube/scrape.ts
+++ src/fetchers/youtube/scrape.ts
@@ -15,7 +15,7 @@ export async function getLivestreamId(env: Env) {
 
 	lastIdFetch = Date.now(); // do this here just in case something requests while below is executing
 	lastId = await realGetLivestreamId();
-	lastIdFetch = Date.now() + (10e3 * Math.random());
+	lastIdFetch = Date.now() + (30e3 * Math.random());
 
 	return lastId;
 }
